451de1bad2484e1d269a8dfed13c3f4b892aa43e
Mariela Hernández Chacón <43797292+hdezmariela@users.noreply.github.com>
2020-09-22 21:05:51 -0600

    Update main_test.cpp

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -15,18 +15,7 @@
 using namespace std;
 
 TEST(TestName, insert_element){
-    list<float> listaGanadora;
 
-    listaGanadora.push_front(2.5);
-    listaGanadora.push_front(16.0);
-    listaGanadora.push_front(55.2);
-    listaGanadora.push_front(42.69);
-    listaGanadora.push_front(0.5);
-
-    struct avl_node nombre = {NULL, NULL, 11};
-    
-    avl_create(&listaGanadora, &nombre);
-    avl_print(nombre);
 }
 
 int main(int argc, char const **argv)
