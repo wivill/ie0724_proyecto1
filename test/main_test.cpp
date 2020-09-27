d14e965fa33d7096fa24a6f4bb5caa0f7cd4b5dc
bernardo <bjza09@hotmail.com>
2020-09-27 17:48:42 -0600

    Inicio pruebas Bernardo

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -55,6 +55,152 @@ TEST(TestName, insert_element){
 
 }
 
+/*
+Test positivo para avl_search
+*/
+
+TEST(positive_avl_search, avl_search){
+    int status;
+    list<float> listaGanadora;
+    listaGanadora.push_front(18.5);
+    listaGanadora.push_front(13.99);
+    listaGanadora.push_front(2.5);
+    listaGanadora.push_front(16.0);
+    listaGanadora.push_front(55.2);
+    listaGanadora.push_front(42.69);
+    listaGanadora.push_front(0.5);
+
+
+    struct avl_node* nombre = new avl_node{NULL, NULL, NULL, 11};
+
+    avl_create(&listaGanadora, nombre);
+    status = avl_search(nombre, 18.5, &nombre);
+    ASSERT_EQ(status, AVL_SUCCESS);
+}
+
+/*
+Test negativo 1 para avl_search, con un parámetro inválido para found_node
+*/
+
+/*
+
+TEST(negative1_avl_search, avl_search){
+    int status;
+    list<float> listaGanadora;
+    listaGanadora.push_front(18.5);
+    listaGanadora.push_front(13.99);
+    listaGanadora.push_front(2.5);
+    listaGanadora.push_front(16.0);
+    listaGanadora.push_front(55.2);
+    listaGanadora.push_front(42.69);
+    listaGanadora.push_front(0.5);
+
+
+    struct avl_node* nombre = new avl_node{NULL, NULL, NULL, 11};
+
+    avl_create(&listaGanadora, nombre);
+    status = avl_search(nombre, 5, NULL);
+    ASSERT_EQ(status, AVL_OUT_OF_RANGE);
+}
+*/
+
+/*
+Test negativo 2 para avl_search, con un parámetro inválido para valor buscado
+*/
+
+TEST(negative2_avl_search, avl_search){
+    int status;
+    list<float> listaGanadora;
+    listaGanadora.push_front(18.5);
+    listaGanadora.push_front(13.99);
+    listaGanadora.push_front(2.5);
+    listaGanadora.push_front(16.0);
+    listaGanadora.push_front(55.2);
+    listaGanadora.push_front(42.69);
+    listaGanadora.push_front(0.5);
+
+
+    struct avl_node* nombre = new avl_node{NULL, NULL, NULL, 11};
+
+    avl_create(&listaGanadora, nombre);
+    status = avl_search(nombre, 0.5, &nombre);
+    ASSERT_EQ(status, AVL_NOT_FOUND);    
+}
+
+
+
+/*
+Test positivo free_avl_tree
+*/
+
+TEST(positive_free_avl_tree, free_avl_tree){
+    int status;
+    list<float> listaGanadora;
+    listaGanadora.push_front(18.5);
+    listaGanadora.push_front(13.99);
+    listaGanadora.push_front(2.5);
+    listaGanadora.push_front(16.0);
+    listaGanadora.push_front(55.2);
+    listaGanadora.push_front(42.69);
+    listaGanadora.push_front(0.5);
+
+
+    struct avl_node* nombre = new avl_node{NULL, NULL, NULL, 11};
+
+    avl_create(&listaGanadora, nombre);
+    status = free_avl_tree(nombre);
+    ASSERT_EQ(status, AVL_SUCCESS);    
+}
+
+
+/*
+Test positivo max_get
+*/
+
+TEST(positive_avl_max_get, avl_max_get){
+    int status;
+    list<float> listaGanadora;
+    listaGanadora.push_front(18.5);
+    listaGanadora.push_front(13.99);
+    listaGanadora.push_front(2.5);
+    listaGanadora.push_front(16.0);
+    listaGanadora.push_front(55.2);
+    listaGanadora.push_front(42.69);
+    listaGanadora.push_front(0.5);
+
+
+    struct avl_node* nombre = new avl_node{NULL, NULL, NULL, 11};
+
+    avl_create(&listaGanadora, nombre);
+    status = avl_max_get(nombre, &nombre);
+    ASSERT_EQ(status, AVL_SUCCESS);    
+}
+
+/*
+Test positivo min_get
+*/
+
+TEST(positive_avl_min_get, avl_min_get){
+    int status;
+    list<float> listaGanadora;
+    listaGanadora.push_front(18.5);
+    listaGanadora.push_front(13.99);
+    listaGanadora.push_front(2.5);
+    listaGanadora.push_front(16.0);
+    listaGanadora.push_front(55.2);
+    listaGanadora.push_front(42.69);
+    listaGanadora.push_front(0.5);
+
+
+    struct avl_node* nombre = new avl_node{NULL, NULL, NULL, 11};
+
+    avl_create(&listaGanadora, nombre);
+    status = avl_min_get(nombre, &nombre);
+    ASSERT_EQ(status, AVL_SUCCESS);    
+}
+
+
+
 int main(int argc, char const **argv)
 {
     testing::InitGoogleTest();
